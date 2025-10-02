7d1c93c43171ead09bb3161d902197520a8b5c4a
Diego Bertosi <diegobertosi@gmail.com>
2025-10-02 18:30:02 -0300

    comprobar

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,6 @@ export default function RootLayout({
               defaultTheme="system"
               enableSystem
               disableTransitionOnChange
-              suppressHydrationWarning
             >
               <AuthProvider>
                 {children}
